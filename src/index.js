7e6214a93df8f96a1c20631dc124a54bef8cd649
Jayson Cheng <jason.cheng@onelab.tw>
2023-01-17 18:47:15 +0800

    2023/01/17 move project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import App from './components/App'
+import rootReducer from './reducers'
+import { composeWithDevTools } from '@redux-devtools/extension';
 
-ReactDOM.render(
-  <React.StrictMode>
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware()
+  )
+)
+
+render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
